e2ecf202e62141feb5483e3a3f1d02f5ccd9bac6
dape <d.m.petersson@gmail.com>
2018-02-21 06:34:53 +0100

    first working version

--- ec2/tools/cloudstrap/main_test.go
+++ ec2/tools/cloudstrap/main_test.go
@@ -6,9 +6,10 @@ import (
 	"testing"
 )
 
+
 func TestExpand(t *testing.T) {
 	cmds := cmdMap{
-		"s3": func(arg string) (string, error) {
+		"s3": func(flag string, arg string) (string, error) {
 			return "s3(" + arg + ")", nil
 		},
 	}
@@ -30,29 +31,29 @@ func TestExpand(t *testing.T) {
 			exec.Command("tree", "--a", "--b", "c"),
 		},
 		{
-			[]string{"--a=s3:A-BUCKET-ID", "--", "tree", "--a", "--b", "c"},
+			[]string{"a=s3:A-BUCKET-ID", "--", "tree", "--a=%a", "--b", "c"},
 			exec.Command("tree", "--a=s3(A-BUCKET-ID)", "--b", "c"),
 		},
 		{
-			[]string{"--a", "s3:A-BUCKET-ID", "--", "tree", "--a", "--b", "c"},
-			exec.Command("tree", "--a", "s3(A-BUCKET-ID)", "--b", "c"),
+			[]string{"a=s3:A-BUCKET-ID", "--", "tree", "--%a", "--b", "c"},
+			exec.Command("tree", "--a=s3(A-BUCKET-ID)", "--b", "c"),
 		},
 		{
-			[]string{"--d", "s3:A-BUCKET-ID", "--", "tree", "--a", "--b", "c", "--d"},
-			exec.Command("tree", "--a", "--b", "c", "--d", "s3(A-BUCKET-ID)"),
-		},
-		{
-			[]string{"--d=s3:A-BUCKET-ID", "--", "tree", "--a", "--b", "c", "--d"},
-			exec.Command("tree", "--a", "--b", "c", "--d=s3(A-BUCKET-ID)"),
-		},
-		{
-			[]string{"--e=s3:A", "--e", "s3:B", "--", "tree", "--e", "--b", "c", "--e", "-x", "--e"},
+			[]string{"e=s3:A", "e=s3:B", "--", "tree", "--e=%e", "--b", "c", "--e", "%e", "-x", "--e=%e"},
 			exec.Command("tree", "--e=s3(A)", "--b", "c", "--e", "s3(B)", "-x", "--e=s3(A)"),
+		},
+		{
+			[]string{"a=s3:A", "name=s3:hello", "--", "tree", "%name", "--a=%a"},
+			exec.Command("tree", "s3(hello)", "--a=s3(A)"),
+		},
+		{
+			[]string{"a=s3:A", "name=s3:hello", "--", "tree", "--x", "%name"},
+			exec.Command("tree", "--x", "s3(hello)"),
 		},
 	}
 
 	for _, tc := range tests {
-		t.Log(tc.cmdLine)
+		t.Logf("%+v", tc)
 		cmd, err := expand(tc.cmdLine, cmds)
 		if err != nil {
 			t.Fatal(err)
@@ -62,13 +63,13 @@ func TestExpand(t *testing.T) {
 
 		if cmd.Path != tc.cmd.Path {
 			t.Log("w", tc.cmd.Path)
-			t.Fatal()
+			t.Fatal("bad path")
 		}
 
 		if !reflect.DeepEqual(cmd.Args, tc.cmd.Args) {
 			t.Log("w", tc.cmd.Args)
-			t.Fatal()
+			t.Fatal("bad args")
 		}
-		t.Log()
+		t.Log("OK")
 	}
 }
