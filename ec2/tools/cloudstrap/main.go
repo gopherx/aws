5862dbb3936e3e0d6a2bb0ae7743bf5d5561514f
dape-wg2 <34238363+dape-wg2@users.noreply.github.com>
2018-02-21 06:35:44 +0100

    Feature/first working version (#2)

--- ec2/tools/cloudstrap/main.go
+++ ec2/tools/cloudstrap/main.go
@@ -12,47 +12,72 @@ import (
 	"github.com/gopherx/base/flag"
 )
 
-type cmdFunc func(arg string) (string, error)
+type cmdFunc func(flag string, arg string) (string, error)
 
 type cmdMap map[string]cmdFunc
 
 var (
 	cmds = map[string]cmdFunc{
-		"s3":  s3download,
-		"GET": httpGet,
+		"s3":            s3download,
+		"http_get":      bytes2string(httpGet),
+		"file_http_get": bytes2file(httpGet),
 	}
 )
 
-func s3download(id string) (string, error) {
+func s3download(flag string, id string) (string, error) {
 	return "", nil
 }
 
-func httpGet(url string) (string, error) {
+func bytes2string(fn func(flag string, url string) ([]byte, error)) func(flag string, url string) (string, error) {
+	return func(flag string, url string) (string, error) {
+		b, err := fn(flag, url)
+		return string(b), err
+	}
+}
+
+func httpGet(flag string, url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return "", errors.InvalidArgument(nil, "GET failed", url, resp.StatusCode, resp)
+		return nil, errors.InvalidArgument(nil, "GET failed", url, resp.StatusCode, resp)
 	}
 
 	bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(bytes), nil
+	return bytes, nil
+}
+
+func bytes2file(fn func(flag string, url string) ([]byte, error)) func(flag string, url string) (string, error) {
+	return func(flag string, url string) (string, error) {
+		b, err := fn(flag, url)
+		if err != nil {
+			return "", err
+		}
+
+		f, err := ioutil.TempFile(".", flag)
+		if err != nil {
+			return "", err
+		}
+
+		return f.Name(), ioutil.WriteFile(f.Name(), b, 0644)
+	}
 }
 
 type expando struct {
-	flag flag.Spec
+	//flag flag.Spec
+	name string
 	cmd  cmdFunc
 	arg  string
 }
 
 func (e *expando) expand() (string, error) {
-	result, err := e.cmd(e.arg)
+	result, err := e.cmd(e.name, e.arg)
 	if err != nil {
 		return "", err
 	}
@@ -63,6 +88,10 @@ func scan(args []string, cmds cmdMap) ([]string, []*expando, error) {
 	exps := []*expando{}
 
 	cmd, err := flag.Scan(args, func(f flag.Spec) error {
+		if len(f.Header) > 0 || f.Separator != "=" {
+			return errors.InvalidArgument(nil, "only key=cmd:arg arguments are supported", f)
+		}
+
 		sepAt := strings.IndexByte(f.Value, ':')
 		if sepAt < 0 {
 			return errors.InvalidArgument(nil, "malformed flag; no command specified", f)
@@ -75,7 +104,7 @@ func scan(args []string, cmds cmdMap) ([]string, []*expando, error) {
 		}
 
 		arg := f.Value[sepAt+1:]
-		exps = append(exps, &expando{f, cmd, arg})
+		exps = append(exps, &expando{f.Name, cmd, arg})
 		return nil
 	})
 
@@ -83,6 +112,12 @@ func scan(args []string, cmds cmdMap) ([]string, []*expando, error) {
 }
 
 func appendFlag(args []string, f flag.Spec) []string {
+	if len(f.Header) == 0 {
+		//...a flag without prefix; only append the value
+		return append(args, f.Value)
+	}
+
+	//...a normal '-' prefixed flag.
 	tmp := fmt.Sprint(f.Header, f.Name)
 	if f.Separator == "=" {
 		tmp = fmt.Sprint(tmp, "=", f.Value)
@@ -90,59 +125,99 @@ func appendFlag(args []string, f flag.Spec) []string {
 
 	args = append(args, tmp)
 
-	if f.Separator == " " {
+	if f.Separator != "=" && len(f.Value) > 0 {
 		args = append(args, f.Value)
 	}
 	return args
 }
 
-func appendExpando(args []string, e *expando) ([]string, error) {
-	res, err := e.expand()
-	if err != nil {
-		return nil, err
-	}
-
-	f := e.flag
-	f.Value = res
-	return appendFlag(args, f), nil
-}
-
-func buildCmd(rem []string, exps []*expando) (*exec.Cmd, error) {
+// buildCmd generates the command from the flags found in rem and the provided expandos.
+// The rem varable may contain the same flag multiple times; the assigned value will be
+// the matching expando until all expandos are used when this happens the values from the
+// expandos will be used in a round robin fashion.
+func buildCmd(rem []string, exps expandoMap) (*exec.Cmd, error) {
 	args := []string{}
-
-	if len(rem) > 1 {
-		var err error
-		_, err = flag.Scan(rem[1:], func(f flag.Spec) error {
-
-			if len(exps) > 0 && exps[0].flag.Name == f.Name {
-				args, err = appendExpando(args, exps[0])
-				exps = exps[1:]
-			} else {
-				args = appendFlag(args, f)
+	_, err := flag.Scan(rem[1:], func(f flag.Spec) error {
+		if len(f.Header) > 0 && !strings.HasPrefix(f.Name, "%") && strings.HasPrefix(f.Value, "%") {
+			// '--x=%name' type expansion
+			eb, ok := exps[strings.TrimPrefix(f.Value, "%")]
+			if !ok {
+				return errors.InvalidArgument(nil, "unknown key", f)
 			}
 
+			ef := f
+			var err error
+			ef.Value, err = eb.next().expand()
+			if err != nil {
+				return err
+			}
+			args = appendFlag(args, ef)
 			return nil
-		})
-
-		if err != nil {
-			return nil, err
 		}
-	}
 
-	var err error
-	for _, e := range exps {
-		args, err = appendExpando(args, e)
-		if err != nil {
-			return nil, err
+		if len(f.Header) > 0 && !strings.HasPrefix(f.Name, "%") && !strings.HasPrefix(f.Value, "%") {
+			// '--x or --x foo or --x=foo' type flag
+			args = appendFlag(args, f)
+			return nil
 		}
-	}
+
+		if len(f.Header) == 0 && strings.HasPrefix(f.Name, "%") && len(f.Value) == 0 {
+			// '%x' type expansion
+			eb, ok := exps[strings.TrimPrefix(f.Name, "%")]
+			if !ok {
+				return errors.InvalidArgument(nil, "unknown key", f)
+			}
+
+			v, err := eb.next().expand()
+			if err != nil {
+				return err
+			}
+
+			args = append(args, v)
+			return nil
+		}
+
+		if len(f.Header) > 0 && strings.HasPrefix(f.Name, "%") && len(f.Value) == 0 {
+			// '--%x' type shortcut expansion
+			name := strings.TrimPrefix(f.Name, "%")
+			eb, ok := exps[name]
+			if !ok {
+				return errors.InvalidArgument(nil, "unknown key", f)
+			}
+
+			ef := f
+			ef.Name = name
+			ef.Separator = "="
+			var err error
+			ef.Value, err = eb.next().expand()
+			if err != nil {
+				return err
+			}
+			args = appendFlag(args, ef)
+			return nil
+		}
+
+		return errors.InvalidArgument(nil, "unsupported format", f, len(f.Value))
+	})
 
 	cmd := exec.Command(rem[0], args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd, err
-
 }
+
+type expandoBag struct {
+	expandos []*expando
+	n        int
+}
+
+func (e *expandoBag) next() *expando {
+	cur := e.expandos[e.n%len(e.expandos)]
+	e.n++
+	return cur
+}
+
+type expandoMap map[string]*expandoBag
 
 func expand(args []string, cmds cmdMap) (*exec.Cmd, error) {
 	rem, exps, err := scan(args, cmds)
@@ -154,7 +229,18 @@ func expand(args []string, cmds cmdMap) (*exec.Cmd, error) {
 		return nil, errors.InvalidArgument(nil, "no program to execute", args)
 	}
 
-	cmd, err := buildCmd(rem, exps)
+	m := expandoMap{}
+	for _, exp := range exps {
+		tmp, ok := m[exp.name]
+		if !ok {
+			tmp = &expandoBag{}
+			m[exp.name] = tmp
+		}
+
+		tmp.expandos = append(tmp.expandos, exp)
+	}
+
+	cmd, err := buildCmd(rem, m)
 	return cmd, err
 }
 
--- ec2/tools/cloudstrap/main_test.go
+++ ec2/tools/cloudstrap/main_test.go
@@ -6,9 +6,10 @@ import (
 	"testing"
 )
 
+
 func TestExpand(t *testing.T) {
 	cmds := cmdMap{
-		"s3": func(arg string) (string, error) {
+		"s3": func(flag string, arg string) (string, error) {
 			return "s3(" + arg + ")", nil
 		},
 	}
@@ -30,35 +31,45 @@ func TestExpand(t *testing.T) {
 			exec.Command("tree", "--a", "--b", "c"),
 		},
 		{
-			[]string{"--a=s3:A-BUCKET-ID", "--", "tree", "--a", "--b", "c"},
+			[]string{"a=s3:A-BUCKET-ID", "--", "tree", "--a=%a", "--b", "c"},
 			exec.Command("tree", "--a=s3(A-BUCKET-ID)", "--b", "c"),
 		},
 		{
-			[]string{"--a", "s3:A-BUCKET-ID", "--", "tree", "--a", "--b", "c"},
-			exec.Command("tree", "--a", "s3(A-BUCKET-ID)", "--b", "c"),
+			[]string{"a=s3:A-BUCKET-ID", "--", "tree", "--%a", "--b", "c"},
+			exec.Command("tree", "--a=s3(A-BUCKET-ID)", "--b", "c"),
 		},
 		{
-			[]string{"--d", "s3:A-BUCKET-ID", "--", "tree", "--a", "--b", "c"},
-			exec.Command("tree", "--a", "--b", "c", "--d", "s3(A-BUCKET-ID)"),
+			[]string{"e=s3:A", "e=s3:B", "--", "tree", "--e=%e", "--b", "c", "--e", "%e", "-x", "--e=%e"},
+			exec.Command("tree", "--e=s3(A)", "--b", "c", "--e", "s3(B)", "-x", "--e=s3(A)"),
 		},
 		{
-			[]string{"--d=s3:A-BUCKET-ID", "--", "tree", "--a", "--b", "c"},
-			exec.Command("tree", "--a", "--b", "c", "--d=s3(A-BUCKET-ID)"),
+			[]string{"a=s3:A", "name=s3:hello", "--", "tree", "%name", "--a=%a"},
+			exec.Command("tree", "s3(hello)", "--a=s3(A)"),
+		},
+		{
+			[]string{"a=s3:A", "name=s3:hello", "--", "tree", "--x", "%name"},
+			exec.Command("tree", "--x", "s3(hello)"),
 		},
 	}
 
 	for _, tc := range tests {
+		t.Logf("%+v", tc)
 		cmd, err := expand(tc.cmdLine, cmds)
 		if err != nil {
-			t.Fatal(err, tc.cmdLine)
+			t.Fatal(err)
 		}
+		t.Log("g", cmd.Path)
+		t.Log("g", cmd.Args)
 
 		if cmd.Path != tc.cmd.Path {
-			t.Fatal(cmd.Path, tc.cmd.Path)
+			t.Log("w", tc.cmd.Path)
+			t.Fatal("bad path")
 		}
 
 		if !reflect.DeepEqual(cmd.Args, tc.cmd.Args) {
-			t.Fatal(cmd.Args, tc.cmd.Args)
+			t.Log("w", tc.cmd.Args)
+			t.Fatal("bad args")
 		}
+		t.Log("OK")
 	}
 }
